09e375b5b9f8ebb63ecd11d72f7df3cb7bb226c2
hanpama <hanpama@gmail.com>
2018-12-03 20:09:58 +0900

    Move FileUpload type to framework

--- packages/mediaservice/src/types.ts
+++ packages/mediaservice/src/types.ts
@@ -1,27 +1,3 @@
-import { Readable } from 'stream';
-
-
-export interface FileUpload {
-  /**
-   * File name
-   */
-  filename: string;
-  /**
-   * File MIME type. Provided by the client and can’t be trusted.
-   */
-  mimetype: string;
-  /**
-   * File stream transfer encoding
-   */
-  encoding: string;
-  /**
-   * Returns a Node.js readable stream of the file contents, for processing and storing the file.
-   * Multiple calls create independent streams. Throws if called after all resolvers have resolved,
-   * or after an error has interrupted the request.
-   */
-  stream: Readable;
-}
-
 export type MediaConstructor<TMedia extends IMedia> = { new(): TMedia };
 
 export interface IMedia {
