5a1b3d2c5a462a8578b89106342970d00056d9d6
hanpama <hanpama@gmail.com>
2018-12-04 22:17:56 +0900

    Make objectstorage-fs pass the spec test

--- packages/objectstorage-fs/src/index.ts
+++ packages/objectstorage-fs/src/index.ts
@@ -4,8 +4,8 @@ import { promisify } from 'util';
 
 import * as fs from 'fs';
 import * as path from 'path';
-import { mkdirp } from 'fs-extra';
 import uuidv1 from 'uuid/v1';
+import { mkdirp } from 'fs-extra';
 
 
 const readFilePromise = promisify(fs.readFile);
@@ -17,23 +17,44 @@ export interface FSObjectStorageConfigs {
   dir: string;
 }
 
-export class FSObjectStorage extends ObjectStorage {
+interface BucketContext {
+  bucketDir: string;
   indexDir: string;
   contentDir: string;
+}
+
+export class FSObjectStorage extends ObjectStorage {
+  buckets: Map<string, BucketContext> = new Map();
 
   constructor(public configs: FSObjectStorageConfigs) {
     super();
-
-    this.indexDir = path.join(this.configs.dir, 'index');
-    this.contentDir = path.join(this.configs.dir, 'content');
   }
 
   async onBootstrap() {
-    await mkdirp(this.indexDir);
-    await mkdirp(this.contentDir);
+    await mkdirp(this.configs.dir);
   }
 
-  public save(filename: string, content: Readable): Promise<StorageObject> {
+  getBucketContext(bucket: string): BucketContext | undefined {
+    return this.buckets.get(bucket);
+  }
+
+  async createBucketContext(bucket: string): Promise<BucketContext> {
+    const bucketDir = path.join(this.configs.dir, bucket);
+    const indexDir = path.join(bucketDir, 'index');
+    const contentDir = path.join(bucketDir, 'content');
+
+    await mkdirp(bucketDir);
+    await Promise.all([
+      mkdirp(indexDir),
+      mkdirp(contentDir),
+    ]);
+
+    const bucketContext = { indexDir, contentDir, bucketDir };
+    this.buckets.set(bucket, bucketContext);
+    return bucketContext;
+  }
+
+  public save(bucket: string, filename: string, content: Readable): Promise<StorageObject> {
     this.validateFilename(filename);
 
     return new Promise(async (resolve, reject) => {
@@ -41,14 +62,16 @@ export class FSObjectStorage extends ObjectStorage {
 
       let contentFilename = fileId;
 
+      let { indexDir, contentDir } = this.getBucketContext(bucket) || await this.createBucketContext(bucket);
+
       const { ext, name } = path.parse(filename);
       if (name) {
         contentFilename = `${name}-${contentFilename}`;
       } if (ext) {
         contentFilename = `${contentFilename}${ext}`;
       }
-      const indexFile = path.join(this.indexDir, fileId);
-      const contentFile = path.join(this.contentDir, contentFilename);
+      const indexFile = path.join(indexDir, fileId);
+      const contentFile = path.join(contentDir, contentFilename);
 
       try {
         await writeFilePromise(indexFile, JSON.stringify([
@@ -61,7 +84,7 @@ export class FSObjectStorage extends ObjectStorage {
 
       const writeStream = fs.createWriteStream(contentFile);
       content.pipe(writeStream);
-      writeStream.once('finish', () => { resolve(this.get(fileId)); });
+      writeStream.once('finish', () => { resolve(this.get(bucket, fileId)); });
       writeStream.once('error', async (e) => {
         try {
           await unlinkPromise(indexFile);
@@ -71,31 +94,13 @@ export class FSObjectStorage extends ObjectStorage {
     });
   }
 
-  protected async resolveIndex(validId: string): Promise<[string, string]> {
-    try {
-      const index = await readFilePromise(path.join(this.indexDir, validId));
-      return JSON.parse(index.toString());
-    } catch (e) {
-      throw new StorageObjectNotFoundError(validId);
-    }
-  }
+  public async delete(bucket: string, id: string): Promise<void> {
+    let { indexDir, contentDir } = this.getBucketContext(bucket) || await this.createBucketContext(bucket);
 
-  protected validateId(id: string) {
-    const isValidId = /^[\w\d-]+$/.test(id);
-    if (!isValidId) { throw new Error('Invalid File ID'); }
-  }
+    const [filename, contentFilename] = await this.resolveIndex(indexDir, id);
 
-  protected validateFilename(filename: string) {
-    if (/^\.\.?$/.test(filename)) { throw new Error('Invalid Filename'); }
-  }
-
-  public async delete(id: string): Promise<void> {
-    this.validateId(id);
-
-    const [filename, contentFilename] = await this.resolveIndex(id);
-
-    const indexFile = path.join(this.indexDir, id);
-    const contentFile = path.join(this.contentDir, contentFilename);
+    const indexFile = path.join(indexDir, id);
+    const contentFile = path.join(contentDir, contentFilename);
 
     try {
       await unlinkPromise(indexFile);
@@ -110,11 +115,12 @@ export class FSObjectStorage extends ObjectStorage {
     }
   }
 
-  public async get(id: string): Promise<StorageObject> {
-    this.validateId(id);
+  public async get(bucket: string, id: string): Promise<StorageObject> {
 
-    const [filename, contentFilename] = await this.resolveIndex(id);
-    const contentFile = path.join(this.contentDir, contentFilename);
+    let { indexDir, contentDir } = this.getBucketContext(bucket) || await this.createBucketContext(bucket);
+
+    const [filename, contentFilename] = await this.resolveIndex(indexDir, id);
+    const contentFile = path.join(contentDir, contentFilename);
 
     try {
       const { size } = await statPromise(contentFile);
@@ -127,5 +133,24 @@ export class FSObjectStorage extends ObjectStorage {
     } catch (e) {
       throw new StorageObjectNotFoundError(id);
     }
+  }
+
+  protected async resolveIndex(indexDir: string, id: string): Promise<[string, string]> {
+    this.validateId(id);
+    try {
+      const index = await readFilePromise(path.join(indexDir, id));
+      return JSON.parse(index.toString());
+    } catch (e) {
+      throw new StorageObjectNotFoundError(id);
+    }
+  }
+
+  protected validateId(id: string) {
+    const isValidId = /^[\w\d-]+$/.test(id);
+    if (!isValidId) { throw new Error('Invalid File ID'); }
+  }
+
+  protected validateFilename(filename: string) {
+    if (/^\.\.?$/.test(filename)) { throw new Error('Invalid Filename'); }
   }
 }
